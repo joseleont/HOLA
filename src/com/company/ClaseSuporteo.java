d9530abaa928c1c57f59ebfca04354cafa8e5593
José León <jose.leont@pucp.pe>
2020-09-14 23:58:21 -0500

    SOY JUAN ya lo mando

--- src/com/company/ClaseSuporteo.java
+++ src/com/company/ClaseSuporteo.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class ClaseSuporteo {
 
+    private String a;
+
 
     //xdxdxdxdxdx
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+       ClaseSuporteo SUPOR =new ClaseSuporteo();
+
 
     }
 }
